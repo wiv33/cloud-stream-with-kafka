b001fdb301b5e78ead99b1916fde3bd7301c3721
PSK <40161102+wiv33@users.noreply.github.com>
2020-06-13 00:33:49 +0900

    add ignore

---
+++ courier-service/src/test/java/org/psawesome/courierservice/CourierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psawesome.courierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ discount-service/src/test/java/org/psawesome/discountservice/DiscountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psawesome.discountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
